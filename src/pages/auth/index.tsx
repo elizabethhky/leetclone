bc64a4e2b1bd603b77e2632354b61237dca4928b
Elizabeth Hong <lizziehong.sg@gmail.com>
2023-10-20 00:18:45 +0800

    add auth page route guard
    * checks if user is already logged in and redirects to home page, does not allow user to go to auth page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar/Navbar";
 import AuthModal from "@/components/Modals/AuthModal";
 import { authModalState } from "@/atoms/authModalAtom";
 import { useRecoilValue } from "recoil";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
+import { useRouter } from "next/router";
 
 type AuthPageProps = {};
 
 const AuthPage: React.FC<AuthPageProps> = () => {
   const authModal = useRecoilValue(authModalState);
+  const [user, loading, error] = useAuthState(auth);
+  const [pageLoading, setPageLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) router.push("/");
+    if (!loading && !user) setPageLoading(false);
+  }, [user, router, loading]);
+
+  if (pageLoading) return null;
+
   return (
     <div className="bg-white h-screen relative">
       <div className="w-full mx-auto">
